065bde1a7a26b39f402352b1a8eddd54952afc42
Anastasiya Zavrazhneva <zavrazhneva.a@gmail.com>
2020-10-06 11:34:22 +0300

    feat: add reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let newNum = 0;
+    if(n % 10 === 0) {
+        newNum = n / 10;
+    } else if(n < 0) {
+        newNum = n * -1
+    } else {
+        newNum = n
+    }
+    let newString =  String(newNum).split("").reverse().join("");
+    return newString
 }
