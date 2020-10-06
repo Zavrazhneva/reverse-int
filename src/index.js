65d7d1d2e3355bb7331706c71fae2764aaf60607
Anastasiya Zavrazhneva <zavrazhneva.a@gmail.com>
2020-10-06 11:46:43 +0300

    fix: refactor code reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 module.exports = function reverse (n) {
-    let newNum = 0;
-    if(n % 10 === 0) {
-        newNum = n / 10;
-    } else if(n < 0) {
+    let newNum = n;
+    if(n < 0) {
         newNum = n * -1
-    } else {
-        newNum = n
     }
-    let newString =  String(newNum).split("").reverse().join("");
-    return newString
+    return +String(newNum).split("").reverse().join("");
 }
